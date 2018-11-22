db132e3098288284ecb437008b9dc0fd2b07643e
0xyd <davisfreeman1015@gmail.com>
2018-11-22 17:27:19 +0800

    <feat>: basic logger Basic Logger to trace embedding training process.

---
+++ logger.py
@@ -0,0 +1,14 @@
+from gensim.models.callbacks import CallbackAny2Vec
+
+class EpochLogger(CallbackAny2Vec):
+
+	def __init__(self):
+		print("Epoch Logger is prepared.")
+		self.epoch = 0
+
+	def on_epoch_begin(self, model):
+		print("Epoch #{} start".format(self.epoch))
+
+	def on_epoch_end(self, model):
+		print("Epoch #{} end".format(self.epoch))		
+		self.epoch += 1
