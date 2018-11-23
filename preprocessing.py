bc8d9b0dc4e1717d6933a844f1a499f4f8eff34d
0xyd <davisfreeman1015@gmail.com>
2018-11-23 19:07:26 +0800

    <fix>: Duplicate and unconnectable corpus Use set to prevent duplicate corpus issue. Preserve all words without keywords deletion.
    Delete keyword form corpus make new keywords cannot correlate
    with previous keyword correctly.

--- preprocessing.py
+++ preprocessing.py
@@ -13,19 +13,32 @@ def mp_extract_keywords(
 
 	for keyword in keywords:
 
-		corpus[keyword] = []
+		corpus[keyword] = set()
 		kp.add_keyword(keyword, ' ')
 
 		for sentence in sentences:
+
+			if isinstance(sentence, list):
+				sentence = ' '.join(sentence)
+
+			# 20181123 LIN, Y.D. Remove Duplicates
+			if sentence in corpus[keyword]:
+				continue
 
 			keywords_found = kp.extract_keywords(sentence)
 
 			if keywords_found:
 
-				tokens = list(filter(
-					lambda s: s if len(s) > 0 else None, 
-					kp.replace_keywords(sentence).split(' ')))
-				corpus[keyword].append(tokens)
+				# 20181123 LIN, Y.D. Reserved keywords.
+				corpus[keyword].add(sentence)
+
+				# tokens = list(filter(
+				# 	lambda s: s if len(s) > 0 else None, sentence.split(' ')))
+
+				# tokens = list(filter(
+				# 	lambda s: s if len(s) > 0 else None, 
+				# 	kp.replace_keywords(sentence).split(' ')))
+				# corpus[keyword].append(tokens)
 
 		kp.remove_keyword(keyword)
 
@@ -56,20 +69,21 @@ class KeywordCorpusIterator():
 
 class KeywordCorpusFactory():
 
-	def __init__(self, keywords, case_sensitive=False):
+	def __init__(self, keywords, case_sensitive=False, worker=3):
 
 		self.keyword_corpus = KeywordCorpus()
 		self.case_sensitive = case_sensitive
+		self.worker = worker
 
 		for keyword in keywords:
-			self.keyword_corpus[keyword] = []
+			self.keyword_corpus[keyword] = set()
+			# self.keyword_corpus[keyword] = []
 
-	def create(self, sentences, chunksize=256, corpus_worker=3):
+	def _create(self, keywords, sentences, chunksize=256):
 
 		sentences_chunk = []
-		partition_size = chunksize // corpus_worker
-		keywords = list(self.keyword_corpus.keys())
-		corpus_pool = Pool(corpus_worker)
+		partition_size = chunksize // self.worker
+		corpus_pool = Pool(self.worker)
 
 		for i, sentence in enumerate(sentences):
 
@@ -77,9 +91,9 @@ class KeywordCorpusFactory():
 
 				partitions = []
 
-				for i in range(corpus_worker):
+				for i in range(self.worker):
 
-					if i == corpus_worker-1:
+					if i == self.worker-1:
 						partitions.append(
 							sentences_chunk[i*partition_size:])
 					else:
@@ -91,8 +105,13 @@ class KeywordCorpusFactory():
 					((keywords, partition, self.case_sensitive) for partition in partitions))
 
 				for new_corpus in new_corpus_list:
-					for keyword, tokens in new_corpus.items():
-						self.keyword_corpus[keyword].append(tokens)
+					for keyword, sentences in new_corpus.items():
+						self.keyword_corpus[keyword] = \
+							self.keyword_corpus[keyword].union(sentences)
+
+					# for keyword, tokens in new_corpus.items():
+					# 	self.keyword_corpus[keyword].extend(tokens)
+						# self.keyword_corpus[keyword].append(tokens)
 
 				sentences_chunk = []
 
@@ -104,22 +123,84 @@ class KeywordCorpusFactory():
 		if sentences_chunk:
 
 			new_corpus = mp_extract_keywords(keywords, sentences_chunk)
-			for keyword, tokens in new_corpus.items():
-				self.keyword_corpus[keyword].append(tokens)
+			for keyword, sentences in new_corpus.items():
+				self.keyword_corpus[keyword] = \
+					self.keyword_corpus[keyword].union(sentences)
 
+				# self.keyword_corpus[keyword].extend(tokens)
+				# self.keyword_corpus[keyword].append(tokens)
+
+	def create(self, sentences, chunksize=256):
+
+		keywords = list(self.keyword_corpus.keys())
+		self._create(keywords, sentences, chunksize=256)
 		return self.keyword_corpus
-
-
-	def update_keywords(self, keywords):
-
-		for keyword in keywords:
-			if keyword in self.keyword_corpus:
-				raise ValueError("Keyword {} is exist already".format(keyword))
-			else:
-				self.keyword_corpus[keyword] = []
-
-	def update(self, sentences, chunksize=256, corpus_worker=3):
 		
-		pass
+		# for i, sentence in enumerate(sentences):
+
+		# 	if i % (chunksize-1) == 0 and i > 0:
+
+		# 		partitions = []
+
+		# 		for i in range(self.worker):
+
+		# 			if i == self.worker-1:
+		# 				partitions.append(
+		# 					sentences_chunk[i*partition_size:])
+		# 			else:
+		# 				partitions.append(
+		# 					sentences_chunk[i*partition_size:(i+1)*partition_size])
+
+		# 		new_corpus_list = corpus_pool.starmap(
+		# 			mp_extract_keywords, 
+		# 			((keywords, partition, self.case_sensitive) for partition in partitions))
+
+		# 		for new_corpus in new_corpus_list:
+		# 			for keyword, tokens in new_corpus.items():
+		# 				self.keyword_corpus[keyword].append(tokens)
+
+		# 		sentences_chunk = []
+
+		# 	else:
+		# 		sentences_chunk.append(sentence)
+
+		# corpus_pool.close()
+
+		# if sentences_chunk:
+
+		# 	new_corpus = mp_extract_keywords(keywords, sentences_chunk)
+		# 	for keyword, tokens in new_corpus.items():
+		# 		self.keyword_corpus[keyword].append(tokens)
+
+		# return self.keyword_corpus
 
 
+	# def update_keywords(self, keywords):
+
+	# 	for keyword in keywords:
+	# 		if keyword in self.keyword_corpus:
+	# 			raise ValueError("Keyword {} is exist already".format(keyword))
+	# 		else:
+	# 			self.keyword_corpus[keyword] = []
+
+	def update(self, keywords=None, sentences=None, chunksize=256):
+
+		if keywords is None and sentences is None:
+			raise ValueError(
+				'One of parameters between keywords and sentences should not be None')
+		if sentences:
+			self.create(sentences, chunksize)
+
+		if keywords:
+			for keyword in keywords:
+				if keyword in self.keyword_corpus:
+					raise ValueError("Keyword {} is exist already".format(keyword))
+				else:
+					self.keyword_corpus[keyword] = set()
+
+			# Retrieve old tokens and assemble them to sentences.
+			sentences_gen = (
+				sentences for corpus in self.keyword_corpus.values() 
+					for sentences in corpus)
+			
+			self._create(keywords, sentences_gen, chunksize)
--- test_preprocessing.py
+++ test_preprocessing.py
@@ -22,8 +22,7 @@ class TestKeywordCorpusFactory():
 	def test_keyword_corpus(self):
 
 		kcf = KeywordCorpusFactory(['hello', 'world'])
-		assert kcf.keyword_corpus['fuck'] == 'Corpus of Keyword fuck does not exist.'
-
+		assert kcf.keyword_corpus['hi'] == 'Corpus of Keyword hi does not exist.'
 
 	def test_create(self):
 
@@ -35,13 +34,71 @@ class TestKeywordCorpusFactory():
 		keyword_corpus = kcf.create(wc)
 
 		for k in keywords:
-			if k not in keyword_corpus:
+			if keyword_corpus[k] is None:
 				assert False
-			else:
-				for tokens in keyword_corpus[k]:
-					if k in tokens:
-						assert False
 		assert True
+
+	def test_update(self):
+
+		first_keywords = ['Hello', 'World']
+		first_sentences = [
+			['Hello World is our first program'], 
+			['Hello World is our last program']
+		]
+		kcf = KeywordCorpusFactory(first_keywords)
+		keyword_corpus = kcf.create(first_sentences)
+
+		second_sentences = [
+			['Hello World is a fantastic sample'],
+			['Hello World is a fantastic example']
+		]
+		kcf.update(sentences=second_sentences)
+
+		print("keyword_corpus['Hello']:")
+		print(keyword_corpus['Hello'])
+		assert keyword_corpus['Hello'] == set([
+			'Hello World is our first program',
+			'Hello World is our last program',
+			'Hello World is a fantastic sample',
+			'Hello World is a fantastic example'
+		])
+
+		assert keyword_corpus['World'] == set([
+			'Hello World is our first program',
+			'Hello World is our last program',
+			'Hello World is a fantastic sample',
+			'Hello World is a fantastic example'
+		])		
+
+		# assert keyword_corpus['Hello'] == [
+		# 		['Hello', 'World', 'is', 'our', 'first', 'program'],
+		# 		['Hello', 'World', 'is', 'our', 'last', 'program'],
+		# 		['Hello', 'World', 'is', 'a', 'fantastic', 'sample'],
+		# 		['Hello', 'World', 'is', 'a', 'fantastic', 'example']
+		# ]
+
+		# assert keyword_corpus['World'] == [
+		# 		['Hello', 'World', 'is', 'our', 'first', 'program'],
+		# 		['Hello', 'World', 'is', 'our', 'last', 'program'],
+		# 		['Hello', 'World', 'is', 'a', 'fantastic', 'sample'],
+		# 		['Hello', 'World', 'is', 'a', 'fantastic', 'example']
+		# ]
+
+		kcf.update(keywords=['program'])
+
+		print("keyword_corpus['program']:")
+		print(keyword_corpus['program'])
+
+		assert keyword_corpus['program'] == set([
+			'Hello World is our first program',
+			'Hello World is our last program',
+		])
+		# assert keyword_corpus['program'] == [
+		# 	['Hello', 'World', 'is', 'our', 'first', 'program'],
+		# 	['Hello', 'World', 'is', 'our', 'last', 'program'],
+		# 	# ['Hello', 'World', 'is', 'a', 'fantastic', 'sample'],
+		# 	# ['Hello', 'World', 'is', 'a', 'fantastic', 'example']
+		# ]
 
 	# def test_create_keyword_corpus(self):
 
