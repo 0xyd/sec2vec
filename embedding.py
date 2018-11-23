a599890bb80950adc95f447a346350daf7820f9d
0xyd <davisfreeman1015@gmail.com>
2018-11-24 07:19:40 +0800

    <feat>: An Abstract class Sec2Vec Abstract class Sec2Vec provides methods for models to share through inheritance

--- embedding.py
+++ embedding.py
@@ -31,36 +31,9 @@ class SentenceIterator():
 			return ValueError(
 				'Only String or list of string are acceptable.')
 
-class KeywordCorpusFactoryWord2VecMixin(Word2Vec, KeywordCorpusFactory): 
+class Sec2Vec():
 
-	def __init__(
-		self, keywords, sentences, 
-		corpus_worker, corpus_chunksize, case_sensitive, 
-		corpus_file, size, alpha, 
-		window, min_count, max_vocab_size, 
-		sample, seed, workers, 
-		min_alpha, sg, hs, 
-		negative, ns_exponent, cbow_mean, 
-		iter, null_word, trim_rule, 
-		sorted_vocab, batch_words, compute_loss, 
-		max_final_vocab):
-		
-		KeywordCorpusFactory.__init__(self, keywords, case_sensitive, corpus_worker)
-		self.kc = self.create(sentences, corpus_chunksize)
-		self.kv = dict(((keyword, []) for keyword in self.kc.keys()))
-		self.corpus_chunksize = corpus_chunksize
-		
-		Word2Vec.__init__(
-			self, 
-			corpus_file=corpus_file, size=size, 
-			alpha=alpha, window=window, min_count=min_count,
-			max_vocab_size=max_vocab_size, sample=sample, seed=seed, 
-			workers=workers, min_alpha=min_alpha, sg=sg, 
-			hs=hs, negative=negative, ns_exponent=ns_exponent, 
-			cbow_mean=cbow_mean, iter=iter, null_word=null_word, 
-			trim_rule=trim_rule, sorted_vocab=sorted_vocab, batch_words=batch_words, 
-			compute_loss=compute_loss, max_final_vocab=max_final_vocab,
-			callbacks=[epoch_logger])
+	def __init__(self): pass
 
 	def __getitem__(self, word):
 
@@ -68,67 +41,6 @@ class KeywordCorpusFactoryWord2VecMixin(Word2Vec, KeywordCorpusFactory):
 			return self.wv[word]
 		except:
 			return ValueError('{} does not exist.'.format(word))
-		
-
-class KeywordCorpusFactoryFasttextMixin(FastText, KeywordCorpusFactory): 
-
-	def __init__(
-		self, keywords, sentences, 
-		corpus_worker, corpus_chunksize, case_sensitive,
-		window=5, min_count=5, max_vocab_size=None, 
-		sample=0.001, seed=1, workers=cpu_count(), 
-		min_alpha=0.0001, sg=0, hs=0, 
-		negative=5, ns_exponent=0.75, cbow_mean=1, 
-		iter=5, null_word=0, trim_rule=None, 
-		sorted_vocab=1, batch_words=10000, compute_loss=False, 
-		max_final_vocab=None):
-
-		KeywordCorpusFactory.__init__(self, keywords, case_sensitive)
-		self.kc = self.create(sentences, corpus_chunksize, corpus_worker)
-		self.kv = dict(((keyword, []) for keyword in self.kc.keys()))
-		# self.corpus_worker = corpus_worker
-		self.corpus_chunksize = corpus_chunksize
-
-		FastText.__init__(self, 
-			window, min_count, max_vocab_size, 
-			sample, seed, workers, 
-			min_alpha, sg, hs, 
-			negative, ns_exponent, cbow_mean, 
-			iter, null_word, trim_rule, 
-			sorted_vocab, batch_words, compute_loss, 
-			max_final_vocab)
-
-	def __getitem__(self, word):
-		return self.wv[word]
-
-
-class SecWord2Vec(KeywordCorpusFactoryWord2VecMixin):
-
-	def __init__(
-		self, keywords, sentences, 
-		corpus_worker=3, corpus_chunksize=256, case_sensitive=False, 
-		corpus_file=None, size=100, alpha=0.025, 
-		window=5, min_count=5, max_vocab_size=None, 
-		sample=0.001, seed=1, workers=cpu_count(), 
-		min_alpha=0.0001, sg=0, hs=0, 
-		negative=5, ns_exponent=0.75, cbow_mean=1, 
-		iter=5, null_word=0, trim_rule=None, 
-		sorted_vocab=1, batch_words=10000, compute_loss=False, 
-		max_final_vocab=None):
-		
-		super().__init__( 
-			keywords, sentences, corpus_worker, 
-			corpus_chunksize, case_sensitive, corpus_file, 
-			size, alpha, window, 
-			min_count, max_vocab_size, sample, 
-			seed, workers, min_alpha, 
-			sg, hs, negative, 
-			ns_exponent, cbow_mean, iter, 
-			null_word, trim_rule, sorted_vocab,
-			batch_words, compute_loss, max_final_vocab)
-
-		self.build_vocab(
-			(corpus for corpus in KeywordCorpusIterator(self.kc)))
 
 	def _get_vec(self, token):
 
@@ -160,6 +72,95 @@ class SecWord2Vec(KeywordCorpusFactoryWord2VecMixin):
 
 				kv = kv / token_count
 				self.kv[keyword] = kv
+
+class KeywordCorpusFactoryWord2VecMixin(Sec2Vec, Word2Vec, KeywordCorpusFactory): 
+
+	def __init__(
+		self, keywords, sentences, 
+		corpus_worker, corpus_chunksize, case_sensitive, 
+		corpus_file, size, alpha, 
+		window, min_count, max_vocab_size, 
+		sample, seed, workers, 
+		min_alpha, sg, hs, 
+		negative, ns_exponent, cbow_mean, 
+		iter, null_word, trim_rule, 
+		sorted_vocab, batch_words, compute_loss, 
+		max_final_vocab):
+		
+		KeywordCorpusFactory.__init__(self, keywords, case_sensitive, corpus_worker)
+		self.kc = self.create(sentences, corpus_chunksize)
+		self.kv = dict(((keyword, []) for keyword in self.kc.keys()))
+		self.corpus_chunksize = corpus_chunksize
+		
+		Word2Vec.__init__(
+			self, 
+			corpus_file=corpus_file, size=size, 
+			alpha=alpha, window=window, min_count=min_count,
+			max_vocab_size=max_vocab_size, sample=sample, seed=seed, 
+			workers=workers, min_alpha=min_alpha, sg=sg, 
+			hs=hs, negative=negative, ns_exponent=ns_exponent, 
+			cbow_mean=cbow_mean, iter=iter, null_word=null_word, 
+			trim_rule=trim_rule, sorted_vocab=sorted_vocab, batch_words=batch_words, 
+			compute_loss=compute_loss, max_final_vocab=max_final_vocab,
+			callbacks=[epoch_logger])
+
+		
+class KeywordCorpusFactoryFasttextMixin(Sec2Vec, FastText, KeywordCorpusFactory):
+
+	def __init__(
+		self, keywords, sentences, 
+		corpus_worker, corpus_chunksize, case_sensitive,
+		window=5, min_count=5, max_vocab_size=None, 
+		sample=0.001, seed=1, workers=cpu_count(), 
+		min_alpha=0.0001, sg=0, hs=0, 
+		negative=5, ns_exponent=0.75, cbow_mean=1, 
+		iter=5, null_word=0, trim_rule=None, 
+		sorted_vocab=1, batch_words=10000, compute_loss=False, 
+		max_final_vocab=None):
+
+		KeywordCorpusFactory.__init__(self, keywords, case_sensitive)
+		self.kc = self.create(sentences, corpus_chunksize, corpus_worker)
+		self.kv = dict(((keyword, []) for keyword in self.kc.keys()))
+		# self.corpus_worker = corpus_worker
+		self.corpus_chunksize = corpus_chunksize
+
+		FastText.__init__(self, 
+			window, min_count, max_vocab_size, 
+			sample, seed, workers, 
+			min_alpha, sg, hs, 
+			negative, ns_exponent, cbow_mean, 
+			iter, null_word, trim_rule, 
+			sorted_vocab, batch_words, compute_loss, 
+			max_final_vocab)
+
+
+class SecWord2Vec(KeywordCorpusFactoryWord2VecMixin):
+
+	def __init__(
+		self, keywords, sentences, 
+		corpus_worker=3, corpus_chunksize=256, case_sensitive=False, 
+		corpus_file=None, size=100, alpha=0.025, 
+		window=5, min_count=5, max_vocab_size=None, 
+		sample=0.001, seed=1, workers=cpu_count(), 
+		min_alpha=0.0001, sg=0, hs=0, 
+		negative=5, ns_exponent=0.75, cbow_mean=1, 
+		iter=5, null_word=0, trim_rule=None, 
+		sorted_vocab=1, batch_words=10000, compute_loss=False, 
+		max_final_vocab=None):
+		
+		super().__init__( 
+			keywords, sentences, corpus_worker, 
+			corpus_chunksize, case_sensitive, corpus_file, 
+			size, alpha, window, 
+			min_count, max_vocab_size, sample, 
+			seed, workers, min_alpha, 
+			sg, hs, negative, 
+			ns_exponent, cbow_mean, iter, 
+			null_word, trim_rule, sorted_vocab,
+			batch_words, compute_loss, max_final_vocab)
+
+		self.build_vocab(
+			(corpus for corpus in KeywordCorpusIterator(self.kc)))
 
 	def train_embed(
 		self, keywords=None, sentences=None, corpus_file=None, update=False,
