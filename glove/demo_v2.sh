3248dfdf2e4e7834c1c173c14f8aed129c97f983
arvis <arvissu@cyber00.org>
2018-11-25 00:24:50 +0800

    new file:   glove/.DS_Store 	new file:   glove/LICENSE 	new file:   glove/Makefile 	new file:   glove/README.md 	new file:   glove/demo.sh 	new file:   glove/demo_v2.sh 	new file:   glove/eval/matlab/WordLookup.m 	new file:   glove/eval/matlab/evaluate_vectors.m 	new file:   glove/eval/matlab/read_and_evaluate.m 	new file:   glove/eval/octave/WordLookup_octave.m 	new file:   glove/eval/octave/evaluate_vectors_octave.m 	new file:   glove/eval/octave/read_and_evaluate_octave.m 	new file:   glove/eval/python/distance.py 	new file:   glove/eval/python/evaluate.py 	new file:   glove/eval/python/word_analogy.py 	new file:   glove/eval/question-data/capital-common-countries.txt 	new file:   glove/eval/question-data/capital-world.txt 	new file:   glove/eval/question-data/city-in-state.txt 	new file:   glove/eval/question-data/currency.txt 	new file:   glove/eval/question-data/family.txt 	new file:   glove/eval/question-data/gram1-adjective-to-adverb.txt 	new file:   glove/eval/question-data/gram2-opposite.txt 	new file:   glove/eval/question-data/gram3-comparative.txt 	new file:   glove/eval/question-data/gram4-superlative.txt 	new file:   glove/eval/question-data/gram5-present-participle.txt 	new file:   glove/eval/question-data/gram6-nationality-adjective.txt 	new file:   glove/eval/question-data/gram7-past-tense.txt 	new file:   glove/eval/question-data/gram8-plural.txt 	new file:   glove/eval/question-data/gram9-plural-verbs.txt 	new file:   glove/src/README.md 	new file:   glove/src/cooccur.c 	new file:   glove/src/glove.c 	new file:   glove/src/shuffle.c 	new file:   glove/src/vocab_count.c

---
+++ glove/demo_v2.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -e
+
+# Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
+# One optional argument can specify the language used for eval script: matlab, octave or [default] python
+
+#make
+
+
+
+
+for i in "$@"
+do
+case $i in
+-cf=*|--Corpus_File=*)
+Corpus_File="${i#*=}"
+shift # past argument=value
+;;
+
+-vf=*|--Vocab_File=*)
+Vocab_File="${i#*=}"
+shift # past argument=value
+;;
+
+-vmc=*|--Vocab_Min_Count=*)
+Vocab_Min_Count="${i#*=}"
+shift # past argument=value
+;;
+
+-vs=*|--Vector_Size=*)
+Vector_Size="${i#*=}"
+shift # past argument=value
+;;
+
+-w=*|--Window=*)
+Window="${i#*=}"
+shift # past argument=value
+;;
+
+-t=*|--Threads=*)
+Threads="${i#*=}"
+shift # past argument=value
+;;
+
+-irer=*|--iters=*)
+iters="${i#*=}"
+shift # past argument=value
+;;
+
+-xmax=*|--X_max=*)
+X_max="${i#*=}"
+shift # past argument=value
+;;
+
+-m=*|--Memory=*)
+Memory="${i#*=}"
+shift # past argument=value
+;;
+
+*)
+# unknown option
+;;
+esac
+done
+
+
+# ./demo.sh --VOCAB_MIN_COUNT=123 --VECTOR_SIZE=456
+
+
+echo $Corpus_File
+echo $Vocab_File
+echo $Vocab_Min_Count
+echo $Vector_Size
+echo $Window
+echo $Threads
+echo $iters
+echo $X_max
+echo $Memory
+
+verbose=2
+binary=2
+cooccurrence_file=cooccurrence.bin
+cooccurrence_shuf_file=cooccurrence.shuf.bin
+builddir=build
+save_file=vectors
+
+echo "$ $builddir/vocab_count -min-count $Vocab_Min_Count -verbose $verbose < $Corpus_File > $Vocab_File"
+$builddir/vocab_count -min-count $Vocab_Min_Count -verbose $verbose < $Corpus_File > $Vocab_File
+#$BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+
+echo "$ $builddir/cooccur -memory $Memory -vocab-file $Vocab_File -verbose $verbose -window-size $Window < $Corpus_File > $cooccurrence_file"
+$builddir/cooccur -memory $Memory -vocab-file $Vocab_File -verbose $verbose -window-size $Window < $Corpus_File > $cooccurrence_file
+echo "$ $builddir/shuffle -memory $Memory -verbose $verbose < $cooccurrence_file > $cooccurrence_shuf_file"
+$builddir/shuffle -memory $Memory -verbose $verbose < $cooccurrence_file > $cooccurrence_shuf_file
+echo "$ $builddir/glove -save-file $save_file -threads $Threads -input-file $cooccurrence_shuf_file -x-max $X_max -iter $iters -vector-size $Vector_Size -binary $binary -vocab-file $Vocab_File -verbose $verbose"
+$builddir/glove -save-file $save_file -threads $Threads -input-file $cooccurrence_shuf_file -x-max $X_max -iter $iters -vector-size $Vector_Size -binary $binary -vocab-file $Vocab_File -verbose $verbose
+
+
+
+
+
+
+
+exit 0
+
